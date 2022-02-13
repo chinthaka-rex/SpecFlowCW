5123e0afe8407c088017d8e1d3e4bd35e6f3f5bb
chinthaka-rex <cwithanage.c@rex.com>
2022-02-13 20:07:13 +0530

    bs impl

---
+++ SpecFlowCW/AppHooks/ApplicationHooks.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using SpecFlowCW.Drivers;
+
+namespace SpecFlowCW.AppHooks
+{
+    [Binding]
+    public sealed class ApplicationHooks
+    {
+        private readonly ScenarioContext _sContext;
+
+        public ApplicationHooks(ScenarioContext sContext)
+        {
+            _sContext = sContext;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenarioWithTag()
+        {
+            DriverFactory driver = new DriverFactory(_sContext);
+            _sContext.Set(driver,"DriverFactory");
+
+        }
+
+        [BeforeScenario(Order = 1)]
+        public void FirstBeforeScenario()
+        {
+
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            _sContext.Get<IWebDriver>("WebDriver").Quit();
+        }
+    }
+}
--- SpecFlowCW/StepDefinitions/LoginPageFeatureStepDefinitions.cs
+++ SpecFlowCW/StepDefinitions/LoginPageFeatureStepDefinitions.cs
@@ -1,30 +1,40 @@
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SpecFlowCW.Drivers;
 
 namespace SpecFlowCW.StepDefinitions
 {
     [Binding]
     public class LoginPageFeatureStepDefinitions
     {
+        IWebDriver driver;
+        private readonly ScenarioContext _sContext;
+        String _title;
+
+        public LoginPageFeatureStepDefinitions(ScenarioContext sContext)
+        {
+            _sContext = sContext;
+        }
+
         [Given(@"user is on login page")]
         public void GivenUserIsOnLoginPage()
         {
-            IWebDriver driver = new ChromeDriver("C:/Users/PramodChinthaka/Documents/Visual Studio 2022/SpecFlowCW/SpecFlowCW/Support/");
-            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
-            
-
-            driver.Close();
-            
+            driver = _sContext.Get<DriverFactory>("DriverFactory").setup();
+            driver.Url = "https://www.saucedemo.com/";
+            Console.WriteLine(driver.Title);
         }
 
         [When(@"user gets the title of the page")]
         public void WhenUserGetsTheTitleOfThePage()
         {
+            _title = driver.Title;
         }
 
         [Then(@"page title should be ""([^""]*)""")]
         public void ThenPageTitleShouldBe(string loginPageTitle)
         {
+            StringAssert.Contains(loginPageTitle, _title);
         }
     }
 }
