151dc5ea44ac5e403aa3a80d7ecde843f34d723a
chinthaka-rex <cwithanage.c@rex.com>
2022-02-20 14:03:18 +0530

    extent dark mode

--- SpecFlowCW/Settings.Designer.cs
+++ SpecFlowCW/Settings.Designer.cs
@@ -70,5 +70,17 @@ namespace SpecFlowCW {
                 this["bsAccessKey"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("~/../../../../Resources/extent_config.xml")]
+        public string extentConfigXmlPath {
+            get {
+                return ((string)(this["extentConfigXmlPath"]));
+            }
+            set {
+                this["extentConfigXmlPath"] = value;
+            }
+        }
     }
 }
